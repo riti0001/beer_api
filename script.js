6b122b09e61cd169d1e50d2f52a2be6d6b70c92d
Rikke <riti0001@stud.kea.dk>
2024-02-02 10:03:50 +0100

    script

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Let's go!");
+window.addEventListener("DOMContentLoaded", init);
+
+const beerURL = "https://api.punkapi.com/v2/beers";
+
+function init() {
+  console.log("init");
+  fetch(beerURL)
+    .then((response) => {
+      return response.json();
+    })
+    .then(function (json) {
+      console.log("json", json);
+    });
+}
