1ba2c3efd75578e4302b259674584dae43fa76fa
Rikke <riti0001@stud.kea.dk>
2024-02-05 20:32:53 +0100

    api

--- script.js
+++ script.js
@@ -29,14 +29,17 @@ function init() {
 }
 
 function showBeers(beerJSON) {
-  console.log("Første json element", beerJSON[0]);
+  let beerClone;
 
-  let beerClone = beerTemplate.cloneNode(true).content;
-  beerClone.querySelector(".beer_image").src = beerJSON[0].image_url;
-  beerClone.querySelector(".beer_image").alt = `Picture of a ${beerJSON[0].image_url} beer`;
+  beerJSON.forEach((beer) => {
+    console.log("Beer", beer);
+    let beerClone = beerTemplate.cloneNode(true).content;
 
-  beerClone.querySelector(".beer_name").textContent = beerJSON[0].name;
-  beerClone.querySelector(".beer_tagline").textContent = beerJSON[0].tagline;
-  beerClone.querySelector(".beer_description").textContent = beerJSON[0].description;
-  beerContainer.appendChild(beerClone);
+    beerClone.querySelector(".beer_image").src = beer.image_url;
+    beerClone.querySelector(".beer_image").alt = `Picture of a ${beer.image_url} beer`;
+    beerClone.querySelector(".beer_name").textContent = beer.name;
+    beerClone.querySelector(".beer_tagline").textContent = beer.tagline;
+    beerClone.querySelector(".beer_description").textContent = beer.description;
+    beerContainer.appendChild(beerClone);
+  });
 }
