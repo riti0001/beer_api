0735903c257a14a258a348e46a11333e8cc2445f
Rikke <riti0001@stud.kea.dk>
2024-02-07 11:28:02 +0100

    final beer

--- script.js
+++ script.js
@@ -2,25 +2,16 @@ window.addEventListener("DOMContentLoaded", init);
 
 const beerURL = "https://api.punkapi.com/v2/beers";
 
-let beerJSON;
-// data vi fetcher hedder beerJSON
-// json er generelt
-
 let beerTemplate;
 let beerContainer;
 
 function init() {
-  console.log("init");
-
   beerTemplate = document.querySelector(".beer_template");
-  console.log("beer_template", beerTemplate);
 
   beerContainer = document.querySelector(".beer_container");
-  console.log("beer_container", beerContainer);
 
-  //   promise = lovet data - data forsinket for hver then
   fetch(beerURL)
-    .then((response) => {
+    .then(function (response) {
       return response.json();
     })
     .then(function (json) {
@@ -31,15 +22,31 @@ function init() {
 function showBeers(beerJSON) {
   let beerClone;
 
+  console.log("beerJSON", beerJSON[0]);
+
   beerJSON.forEach((beer) => {
-    console.log("Beer", beer);
-    let beerClone = beerTemplate.cloneNode(true).content;
+    beerClone = beerTemplate.cloneNode(true).content;
+    beerClone.querySelector(".beer_link").href = `beer.html?id=${beer.id}`;
+    beerClone.querySelector(".beer_abv_data").textContent = beer.abv;
+
+    if (beer.abv >= 5.5) {
+      beerClone.querySelector(".beer_strong").classList.remove("hide");
+      beerClone.querySelector(".beer_article").classList.add("strong");
+    } else if (beer.abv <= 4.4) {
+      beerClone.querySelector(".beer_article").classList.add("mild");
+    }
+
+    if (beer.method.twist) {
+      beerClone.querySelector(".beer_twist").classList.remove("hide");
+      beerClone.querySelector(".beer_article").classList.add("twist");
+    }
 
     beerClone.querySelector(".beer_image").src = beer.image_url;
-    beerClone.querySelector(".beer_image").alt = `Picture of a ${beer.image_url} beer`;
+    beerClone.querySelector(".beer_image").alt = `Picture of a ${beer.name} beer`;
     beerClone.querySelector(".beer_name").textContent = beer.name;
     beerClone.querySelector(".beer_tagline").textContent = beer.tagline;
     beerClone.querySelector(".beer_description").textContent = beer.description;
+
     beerContainer.appendChild(beerClone);
   });
 }
